aa5c38207a2eaa2bca4408a308c94d69e48d7aa5
TrongDuc1 <121988726+TrongDuc1@users.noreply.github.com>
2023-01-11 19:36:07 +0700

    Add files via upload

---
+++ bai1.kt
@@ -0,0 +1,21 @@
+enum class Signal {
+    WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP
+}
+fun calculateHandshake(n: Int): List<Signal> =
+    if (n >= 16)
+        calculateHandshake(n - 16).reversed()
+    else if (n >= 8)
+        calculateHandshake(n - 8) + listOf(Signal.JUMP)
+    else if (n >= 4)
+        calculateHandshake(n - 4) + listOf(Signal.CLOSE_YOUR_EYES)
+    else if (n >= 2)
+        calculateHandshake(n - 2) + listOf(Signal.DOUBLE_BLINK)
+    else if (n >= 1)
+        listOf(Signal.WINK)
+    else
+        listOf()
+fun main() {
+    print("Enter a decimal number: ")
+    val n: Int = readLine()!!.toInt()
+    print("${calculateHandshake(n)}")
+}
